8253a69b0f3300e6c92301c48af42dd7d96a67da
Chau Dinh <chaudvb95uc@gmail.com>
2019-07-04 00:49:54 +0700

    Data Structures: Initial commit

---
+++ Data Structures/Linked Lists/index.js
@@ -0,0 +1,52 @@
+// const n1 = {
+//   data: 100
+// };
+
+// const n2 = {
+//   data: 200
+// };
+
+// n1.next = n2;
+
+// console.log(n1);
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+const n1 = new Node(100);
+
+console.log(n1);
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Insert first node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  // Insert last node
+
+  // Insert at index
+
+  // Get at index
+
+  // Remove at index
+
+  // Clear the list
+
+  // Print the list data
+}
+
+const ll = new LinkedList();
+
+ll.insertFirst(100);
+
+console.log(ll);
